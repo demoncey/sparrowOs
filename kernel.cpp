690ebbd58f698f18704a7623c5a4b1f778401b20
demoncey <demoncey@gmail.com>
2018-05-20 21:35:06 +0200

    fix

--- kernel.cpp
+++ kernel.cpp
@@ -1,21 +1,19 @@
 #include "kernel.h"
 
-SpKernel *instance=0;
-
-
+SpKernel *SpKernel::instance=0;
 
 void SpKernel::checkIfworks(){
 	SERIAL_LOGGER("works fine!!!!!");
-};
+}
 
 SpKernel* SpKernel::getInstance(){
   if(instance){
     instance=new SpKernel();
   }
   return instance;
-};
+}
 
-SpKernel::SpKernel(){};
+SpKernel::SpKernel(){}
 
 
 
