28be05fe9228d5d9d292fb1ecf5310e619c8f151
demoncey <demoncey@gmail.com>
2018-05-21 22:43:50 +0200

    new macro added

--- kernel.h
+++ kernel.h
@@ -6,8 +6,8 @@
 
 
 #define SERIAL_LOGGER(msg) (Serial)?Serial.println((msg)):0
-#define GET_HEX_PTR(ptr) (uint16_t)(ptr)
-
+#define GET_UINT_PTR(ptr) (uint16_t)(ptr)
+#define GET_HEX_PTR(ptr) String((uint16_t)(ptr),HEX)
 
 class Kernel
 {
