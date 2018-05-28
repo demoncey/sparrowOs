f0272db40d587ac566556b21575a520044c56159
demoncey <demoncey@gmail.com>
2018-05-28 20:08:14 +0200

    string fixed

--- logger.h
+++ logger.h
@@ -13,7 +13,7 @@
 
 void showMemory(){
   extern int __heap_start, *__brkval; 
-  SERIAL_LOGGER(string("__brkval addr: ")+GET_HEX_PTR(__brkval));
-  SERIAL_LOGGER(string("__heap_start addr: ")+GET_HEX_PTR(&__heap_start));
+  SERIAL_LOGGER(String("__brkval addr: ")+GET_HEX_PTR(__brkval));
+  SERIAL_LOGGER(String("__heap_start addr: ")+GET_HEX_PTR(&__heap_start));
 }
 #endif /* LOGGER_H_ */
