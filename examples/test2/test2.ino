56b16adb8c0ff18974eceb8fb9859b7beedc83b8
demoncey <demoncey@gmail.com>
2018-05-28 19:30:12 +0200

    second kernel fixed

--- examples/test2/test2.ino
+++ examples/test2/test2.ino
@@ -1,12 +1,14 @@
 #include <kernelx.h>
 
+KernelX a;
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
 
 }
 
 void loop() {
-  //SERIAL_LOGGERX(GET_HEX_PTRX(&KernelX::getInstance()));
+  SERIAL_LOGGERX(GET_HEX_PTRX(&KernelX::getInstance()));
   SERIAL_LOGGERX("DDDDD");
   delay(1000);
 }
